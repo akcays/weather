6061f75c9b09c19045f0f0baf7333b5976d44f7c
akcays <ilkesedefakcay@gmail.com>
2017-09-05 16:44:25 -0700

    Build action creator to fetch weather

---
+++ src/actions/index.js
@@ -0,0 +1,16 @@
+import axios from 'axios';
+import config from '../config/config';
+
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${config.API_KEY}`;
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather(city, country='us') {
+  const url = `${ROOT_URL}&q=${city},${country}`;
+  const request = axios.get(url);
+
+  return {
+    type: FETCH_WEATHER,
+    payload: request
+  };
+}
